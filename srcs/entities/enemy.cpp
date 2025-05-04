a6c91529d19715a1627850f68777a4d4671243a4
StanX19 <tanshanchien@gmail.com>
2025-05-05 00:00:04 +0800

    working version

--- srcs/entities/enemy.cpp
+++ srcs/entities/enemy.cpp
@@ -7,6 +7,6 @@ void spawn_enemy(entt::registry& registry, const Vector3& pos) {
     registry.emplace<Rotation>(enemy, Vector3{ 0, 0, 0 });
     registry.emplace<Body>(enemy, 1.0f, GREEN);
     registry.emplace<HP>(enemy, 100.0f, 100.0f);
-    registry.emplace<Damage>(enemy, 25.0f);
+    registry.emplace<Damage>(enemy, 50.0f);
     registry.emplace<Enemy>(enemy, ENEMY_SPEED, ENEMY_ATTACK_COOLDOWN, 0.0f);
 }
