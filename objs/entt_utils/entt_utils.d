af465bbf9f7af0c93407571b3983a132a785f26a
StanX19 <tanshanchien@gmail.com>
2025-06-10 18:02:07 +0800

    clean

--- objs/entt_utils/entt_utils.d
+++
@@ -1,8 +0,0 @@
-objs/entt_utils/entt_utils.o: srcs/entt_utils/entt_utils.cpp \
- headers/entt_utils.hpp headers/includes.hpp includes/raylib/raylib.h \
- includes/raylib/raymath.h includes/entt/entt.hpp
-headers/entt_utils.hpp:
-headers/includes.hpp:
-includes/raylib/raylib.h:
-includes/raylib/raymath.h:
-includes/entt/entt.hpp:
--- objs/utils/path_utils.d
+++
@@ -1,9 +0,0 @@
-objs/utils/path_utils.o: srcs/utils/path_utils.cpp headers/utils.hpp \
- headers/includes.hpp includes/raylib/raylib.h includes/raylib/raymath.h \
- includes/entt/entt.hpp headers/components.hpp
-headers/utils.hpp:
-headers/includes.hpp:
-includes/raylib/raylib.h:
-includes/raylib/raymath.h:
-includes/entt/entt.hpp:
-headers/components.hpp:
