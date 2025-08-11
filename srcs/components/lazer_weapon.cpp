5f0e20f8667f09d831afef9f24ac77cf56147ef2
StanX19 <tanshanchien@gmail.com>
2025-08-11 14:49:24 +0800

    balance lazer

--- srcs/components/lazer_weapon.cpp
+++ srcs/components/lazer_weapon.cpp
@@ -4,7 +4,7 @@
 namespace {
 	const float LAZER_SPEED = 10000.0f;
 	const float LAZER_LIFETIME = 1.0f;
-	const float BASE_DAMAGE = 10.0f;
+	const float BASE_DAMAGE = 5.0f;
 	const float BASE_SPREAD = 0.05f * DEG2RAD;
 
 	Color getColor([[maybe_unused]] GameContext &context, [[maybe_unused]] entt::entity entity, Color baseColor = WHITE)
@@ -40,7 +40,7 @@ void emplaceWeaponLazerMachineGun(GameContext &context, entt::entity entity)
 	BulletWeapon weapon;
 
 	weapon.bulletData.hp = 1.0f;
-	weapon.bulletData.dmg = BASE_DAMAGE * 0.2f;
+	weapon.bulletData.dmg = BASE_DAMAGE * 0.4f;
 	weapon.bulletData.speed = LAZER_SPEED;
 	weapon.bulletData.rad = 0.1f;
 	weapon.bulletData.color = getColor(context, entity, GREEN);
@@ -48,9 +48,9 @@ void emplaceWeaponLazerMachineGun(GameContext &context, entt::entity entity)
 	weapon.bulletData.spreadSin = std::sin(BASE_SPREAD * 10.0f);
 
 	context.registry.emplace_or_replace<BulletWeapon>(entity, weapon);
-	context.registry.emplace_or_replace<Ammo>(entity, Ammo{20.0f, 75});
-	context.registry.emplace_or_replace<AmmoReload>(entity, AmmoReload{15.0});
-	context.registry.emplace_or_replace<WeaponCooldown>(entity, WeaponCooldown{0.001});
+	context.registry.emplace_or_replace<Ammo>(entity, Ammo{30.0f, 60});
+	context.registry.emplace_or_replace<AmmoReload>(entity, AmmoReload{5.0});
+	context.registry.emplace_or_replace<WeaponCooldown>(entity, WeaponCooldown{0.05});
 	context.registry.emplace_or_replace<tag::weapon::IsWeapon>(entity);
 	context.registry.emplace_or_replace<tag::weapon::type::Lazer>(entity);
 	context.registry.emplace_or_replace<AimTarget>(entity);
