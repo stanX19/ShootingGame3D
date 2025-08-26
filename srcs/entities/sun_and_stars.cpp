49ebf2811e775e468aa826de4fc675d4180e3972
StanX19 <tanshanchien@gmail.com>
2025-08-26 17:46:14 +0800

    using sun model

--- srcs/entities/sun_and_stars.cpp
+++ srcs/entities/sun_and_stars.cpp
@@ -6,12 +6,12 @@
 void spawnSunAndStars(GameContext &context, int numStars) {
 	entt::entity sun2 = context.registry.create();
 	Position pos = {randomUnitVector3() * ARENA_SIZE * 3};
-	// t_model_id sunModel = context.modelManager.loadModel("assets/Models/sun/sun.glb");
-	t_model_id sunModel = context.modelManager.createSphere(128, 128);
-	float rad = GetRandomValue(ARENA_SIZE * 0.3, ARENA_SIZE * 0.5);
+	t_model_id sunModel = context.modelManager.loadModel("assets/Models/sun/sun.glb");
+	// t_model_id sunModel = context.modelManager.createSphere(128, 128);
+	float rad = GetRandomValue(150, 250);
 
 	context.registry.emplace<Position>(sun2, pos);
-	context.registry.emplace<RenderBody>(sun2, sunModel, Color{105, 205, 255, 255}, rad);
+	context.registry.emplace<RenderBody>(sun2, sunModel, rad);
 	context.registry.emplace<tag::LightSource>(sun2);
 	
 	// stars
@@ -25,13 +25,13 @@ void spawnSunAndStars(GameContext &context, int numStars) {
 	}
 
 	// space
-	t_model_id spaceModel = context.modelManager.loadModel("assets/Models/space/sky_pano_-_milkyway.glb");
+	t_model_id spaceModel = context.modelManager.loadModel("assets/Models/space/inside_galaxy_skybox_hdri_360_panorama.glb");
 	Model model = context.modelManager.getModel(spaceModel);
 	model.materials[0].maps[MATERIAL_MAP_ALBEDO].texture = model.materials[1].maps[MATERIAL_MAP_EMISSION].texture;
 	model.materials[1].maps[MATERIAL_MAP_ALBEDO].texture = model.materials[1].maps[MATERIAL_MAP_EMISSION].texture;
 
 	entt::entity space = context.registry.create();
 	context.registry.emplace<Position>(space);
-	context.registry.emplace<RenderBody>(space, RenderBody{spaceModel, 1.0f});
+	context.registry.emplace<RenderBody>(space, RenderBody{spaceModel, 100.0f});
 	context.registry.emplace<tag::SkyBox>(space);
 }
